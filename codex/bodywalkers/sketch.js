9ac88d8ff6046640301b14c04bb580ca240321e3
Kyle Stewart <kylestew@gmail.com>
2016-08-20 14:04:42 -0700

    Added template, working on bodywalkers

--- codex/bodywalkers/sketch.js
+++ codex/bodywalkers/sketch.js
@@ -2,46 +2,94 @@ define(function(require) {
   require('lib/dat.gui.min');
   var Superformula = require('codex/superformula');
 
+  var _p5;
   var drawWidth, drawHeight;
   var superformula;
   var points;
   var time;
 
   var params = {
-    shape: "triangle",
+    shape: "circle",
 
-    pointCount: 50,
-    pointWeight: 0.66,
+    // points
+    count: 400,
+    weight: 1.0,
+    strength: 20, // 0 - 100
 
+    // style
     backColor: '#000000',
     color0: '#88b1be',
-    // color1:
-    // color2:
+    color1: '#ff0000',
+    repeat: 2,
+
+    // walking
+    noiseBias: 0.466,
   };
   var createUI = function(sketch, p5) {
     var gui = new dat.GUI();
 
-    gui.add(params, 'shape', Object.keys(superformula.presets)).onChange(function(newShape) {
-      superformula.setPreset(params.shape);
-      points = superformula.generateVectorPointArray(params.pointCount);
+    var shapeKeys = Object.keys(superformula.presets);
+    gui.add(params, 'shape', shapeKeys).onChange(function(newShape) {
+      resetShape();
     });
-    gui.add(params, 'pointCount').min(10).max(1000).step(1);
+
+    var points = gui.addFolder('Points');
+    points.add(params, 'count').min(10).max(1000).step(1);
+    points.add(params, 'weight').min(0.1).max(3);
+    points.add(params, 'strength').min(0).max(100);
 
     var style = gui.addFolder('Style');
     style.addColor(params, 'backColor');
+    style.addColor(params, 'color0');
+    style.addColor(params, 'color1');
+    style.add(params, 'repeat').min(1).max(8).step(1);
 
     sketch.addResetToGUI(gui, params);
     sketch.addSaveToGUI(gui, params);
   }
 
   var setup = function(p5) {
+    _p5 = p5;
     superformula = new Superformula(p5);
+    p5.smooth(8);
+  }
+
+  function resetShape() {
+    // generate shape using selected superformula
+    superformula.setPreset(params.shape);
+    // set size of shape based on window size - not scaling it
+    superformula.a = drawWidth / 2;
+    superformula.b = drawWidth / 2;
+    var dots = superformula.generateVectorPointArray(params.count);
+
+    // convert to point objects and apply colors
+    points = [];
+
+// TODO: colors
+  //   // select color from palette (index based on noise)
+  //   var cn = (int)(100*pal.length*noise(idx))%pal.length;
+  //   stroke(pal[cn]);
+
+    var col0 = _p5.color(params.color0);
+    var col1 = _p5.color(params.color1);
+
+    for (var i = 0; i < dots.length; i++) {
+
+      var ang = _p5.map(i, 0, dots.length, 0, params.repeat * 2 * Math.PI);
+      var col = _p5.lerpColor(col0, col1, Math.sin(ang));
+
+      var point = {
+        x: dots[i].x,
+        y: dots[i].y,
+        color: col,
+      }
+      points.push(point);
+    }
   }
 
   var reset = function(p5) {
     p5.background(params.backColor);
-    p5.strokeWeight(params.pointWeight);
-    p5.noFill();
+    p5.noStroke();
 
     // make centered draw square with borders - for dramatic effect
     if (p5.width > p5.height) {
@@ -54,13 +102,7 @@ define(function(require) {
 
     // reset
     time = 0;
-
-    // generate shape using selected superformula
-    superformula.setPreset(params.shape);
-    // set size of shape based on window size - not scaling it
-    superformula.a = drawWidth / 2;
-    superformula.b = drawWidth / 2;
-    points = superformula.generateVectorPointArray(params.pointCount);
+    resetShape();
   }
 
   var draw = function(p5) {
@@ -71,27 +113,20 @@ define(function(require) {
     for (var i = 0; i < points.length; i++) {
       var p = points[i];
 
-// TODO: colors
-  //   // select color from palette (index based on noise)
-  //   var cn = (int)(100*pal.length*noise(idx))%pal.length;
-  //   stroke(pal[cn]);
-
-      // draw point
-      // p5.stroke(params.color0);
-      p5.stroke(255, 255, 255, 100);
-      p5.strokeWeight(10);
-      p5.point(p.x, p.y);
+      // draw point with assigned color/weight/strength
+      var col = p5.color(p5.red(p.color), p5.green(p.color), p5.blue(p.color), params.strength);
+      p5.fill(col);
+      p5.ellipse(p.x, p.y, params.weight);
 
       // apply noise
-      // TODO: apply alpha
-      // TODO: lines for prev position - not points
-      // p.x += 12.8 * (p5.noise(p.x, p.y, time) - 0.5);
-      // p.y += 20.0 * (p5.noise(p.x, p.y, time) - 0.5);
+      // TODO: perlin noise displaying nasty semetry
+      p.x += 20.0 * (p5.noise(p.x, p.y, time) - params.noiseBias);
+      p.y += 20.0 * (p5.noise(p.x, p.y, time) - params.noiseBias);
     }
 
-    time += 0.001;
+    // time += 0.001;
   }
 
   var p5Boiler = require('codex/p5boiler');
-  var sketch = p5Boiler(10, setup, createUI, reset, draw);
+  var sketch = p5Boiler(60, setup, createUI, reset, draw);
 });
---
+++ codex/p5template/sketch.js
@@ -0,0 +1,86 @@
+define(function(require) {
+  require('lib/dat.gui.min');
+  var Superformula = require('codex/superformula');
+
+  var drawWidth, drawHeight;
+  var counts;
+  var time;
+
+  var params = {
+    bias: 0.466,
+  };
+  var createUI = function(sketch, p5) {
+    var gui = new dat.GUI();
+
+    gui.add(params, 'bias').min(0).max(1);
+
+    sketch.addResetToGUI(gui, params);
+    sketch.addSaveToGUI(gui, params);
+  }
+
+  var setup = function(p5) {
+    p5.noiseSeed(0);
+    p5.noiseDetail(8, 0.65);
+  }
+
+  var reset = function(p5) {
+    // make centered draw square with borders - for dramatic effect
+    if (p5.width > p5.height) {
+      drawHeight = p5.floor(p5.height * 0.6);
+      drawWidth = drawHeight;
+    } else {
+      drawWidth = p5.floor(p5.width * 0.6);
+      drawHeight = drawWidth;
+    }
+
+    // reset
+    time = 0;
+
+    // subdivide drawWidth by slots to count into
+    counts = [];
+    for (var i = 0; i < drawWidth; ++i)
+      counts.push(0);
+  }
+
+  var draw = function(p5) {
+    p5.background(0);
+    p5.strokeWeight(1);
+    p5.stroke(255);
+    p5.noFill();
+
+    // center drawing
+    p5.translate((p5.width - drawWidth) / 2, p5.height/2);
+    // make graph mode by inverting y axis
+    p5.scale(1, -1);
+
+    // draw counts
+    for (var i = 0; i < counts.length; i++) {
+      // perlin
+      counts[i] += 0.5 * (p5.noise(i, time) - params.bias);
+
+      var len = counts[i];
+
+      p5.line(i, 0, i, len);
+    }
+
+    // rough idea of imbalance
+    var above = 0;
+    var below = 0;
+    for (var i = 0; i < counts.length; i++) {
+      if (counts[i] > 0)
+        above += counts[i];
+      else
+        below += Math.abs(counts[i]);
+    }
+    if (above > below) {
+      console.log("above :: " + (above - below));
+    } else {
+      console.log("below :: " + (below - above));
+    }
+
+    time += 0.001;
+  }
+
+  var p5Boiler = require('codex/p5boiler');
+  var sketch = p5Boiler(60, setup, createUI, reset, draw);
+});
